36f02c5c5c9f5592382f3643511fc55bebe261b4
MunkaBest <66256440+MunkaBest@users.noreply.github.com>
2023-05-11 19:12:50 +0700

    Firebase assets full clear

--- Assets/TopBeton/Script/CetListCreator.cs
+++ Assets/TopBeton/Script/CetListCreator.cs
@@ -1,5 +1,3 @@
-using Firebase.Database;
-using Firebase.Extensions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,20 +6,7 @@ public class CetListCreator : MonoBehaviour
 {
     void Start()
     {
-        FirebaseDatabase.DefaultInstance
-       .GetReference("CarList")
-       .GetValueAsync().ContinueWithOnMainThread(task =>
-       {
-           if (task.IsFaulted)
-           {
-               Debug.Log(task);
-           }
-           else if (task.IsCompleted)
-           {
-               DataSnapshot snapshot = task.Result;
-               Debug.Log(snapshot.GetRawJsonValue());
-           }
-       });
+
     }
 
 }
